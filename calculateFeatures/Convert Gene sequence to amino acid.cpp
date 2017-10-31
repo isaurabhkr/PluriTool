9fd70a300eb0e810841395a2d8b2e4ecc30c8d30
Vikas Choudhary <dragovikas@gmail.com>
2017-10-31 21:53:45 +0530

    Update Convert Gene sequence to amino acid.cpp

--- calculateFeatures/Convert Gene sequence to amino acid.cpp
+++ calculateFeatures/Convert Gene sequence to amino acid.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <algorithm>
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
 char ch;
 string line, name, content;
