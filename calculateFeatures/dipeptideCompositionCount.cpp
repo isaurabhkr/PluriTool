27de4672e1a3b2d3424004eeb17a00492b0c26d0
kaioken7 <33261672+kaioken7@users.noreply.github.com>
2017-10-31 22:05:12 +0530

    Update dipeptideCompositionCount.cpp

--- calculateFeatures/dipeptideCompositionCount.cpp
+++ calculateFeatures/dipeptideCompositionCount.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <algorithm>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 char ch,ch1,ch3;
 string line, name, content;
